e62f358e4178b8a69fc49ba6140607af669231ca
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-08-19 14:17:42 +0200

    feat: read inputs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { globSync } from 'node:fs';
+
+try {
+  const fileGlob = core.getInput('files', { required: true });
+
+  const files = globSync(fileGlob);
+
+  core.info(`Files found: ${files.join(', ')}`);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  core.info(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
